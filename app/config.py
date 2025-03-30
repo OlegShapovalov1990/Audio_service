b7c3ac7595e4ff983f972f31b6b051d5b770f8ff
oleg9 <olega_djan@mail.ru>
2025-03-30 17:06:15 +0300

    add @property: DATABASE_URL

--- app/config.py
+++ app/config.py
@@ -1,8 +1,11 @@
 from pydantic_settings import BaseSettings
+from pydantic import PostgresDsn, computed_field
 from functools import lru_cache
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
@@ -10,8 +13,14 @@ class Settings(BaseSettings):
     YANDEX_CLIENT_SECRET: str
     YANDEX_REDIRECT_URI: str
 
+    @computed_field
+    @property
+    def DATABASE_URL(self) -> PostgresDsn:
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@localhost:5432/{self.POSTGRES_DB}"
+
     class Config:
         env_file = ".env"
+        extra = "forbid"  # Запрещает лишние поля
 
 @lru_cache()
 def get_settings():
